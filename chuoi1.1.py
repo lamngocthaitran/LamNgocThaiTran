a58383f8834a2396f7c0e3ba93d1fc1f6a7f3246
lamngocthaitran <lamngocthaitran1403@gmail.com>
2024-08-22 20:19:14 +0700

    Add files via upload

---
+++ chuoi1.1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 20:17:42 2024
+
+@author: lamngocthaitran
+"""
+str_name=("Đại học quốc gia, khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM")
+print(str_name.replace(", ","\n").replace(", ","\n").replace(", ","\n"))
---
+++ chuoi1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 19:21:44 2024
+
+@author: lamngocthaitran
+"""
+
+str_name=("Đại học quốc gia, khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM")
+print(str_name.replace(", P.","\n").replace(", Q.","\n").replace(", TP.","\n"))
